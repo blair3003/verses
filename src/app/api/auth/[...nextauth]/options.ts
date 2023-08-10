e4e989288ac432d3e2fe6fbe3362417c58bff7ba
Blair Fleming <blair3003.bf@gmail.com>
2023-08-10 19:14:37 +0100

    Initial commit

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,22 @@
+import type { AuthOptions } from 'next-auth'
+
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import clientPromise from '@/app/mongodb/clientPromise'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const options: AuthOptions = {
+    adapter: MongoDBAdapter(clientPromise),
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === 'development',
+}
