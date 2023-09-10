2d266d1bd8fa49756494f4d4e77548e82d5101bb
Blair Fleming <blair3003.bf@gmail.com>
2023-09-10 11:24:49 +0100

    group user search

--- src/app/v/new/group/components/NewGroupForm.tsx
+++ src/app/v/new/group/components/NewGroupForm.tsx
@@ -7,6 +7,7 @@ import { HiArrowRight } from 'react-icons/hi2'
 import { useSession } from 'next-auth/react'
 import ProfilePic from '@/app/components/ProfilePic'
 import { PulseLoader } from 'react-spinners'
+import SearchUser from '../../components/SearchUser'
 
 interface Props {
     users: User[]
@@ -14,10 +15,13 @@ interface Props {
 
 const NewGroupForm = ({ users }: Props) => {
 
-    const { register, handleSubmit } = useForm<FieldValues>()
     const [isLoading, setIsLoading] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('')
     const router = useRouter()
     const { update } = useSession()
+    const { register, handleSubmit, reset } = useForm<FieldValues>()
+
+    const filteredUsers = searchTerm ? users.filter(user => user.name.toLowerCase().includes(searchTerm.toLowerCase())) : users
 
     const onSubmit: SubmitHandler<FieldValues> = async (data) => {
         
@@ -32,9 +36,6 @@ const NewGroupForm = ({ users }: Props) => {
             }
 
             if (userIds.length) {
-                console.log(`creating a group with name: ${data.name}`)
-                console.log(`creating a group with userIds: ${userIds}`)
-
                 const group = await fetch('/api/groups', {
                     method: 'POST',
                     body: JSON.stringify({
@@ -44,14 +45,11 @@ const NewGroupForm = ({ users }: Props) => {
                 })
 
                 const verseId = await group.json()
-
                 if (verseId) {
                     await update({ verseId })
                     router.push(`/v/${verseId}`)
                 }
-
             }
-
 
         } catch (err) {
             console.error(err)
@@ -78,15 +76,24 @@ const NewGroupForm = ({ users }: Props) => {
                         type="text"
                         placeholder="Group name"
                         {...register('name', { required: true })}
-                        className="text-white bg-transparent border-b-2 border-gray-900 w-full"
+                        className="text-white bg-transparent border-b-2 border-gray-900 w-full p-2"
                     />
                 </div>
 
                 <div>
-                    <div className="text-base text-gray-300 mb-4">
-                        Participants
+                    <div className="mb-4 flex items-center justify-between">
+                        <div className="text-base text-gray-300">
+                            Participants
+                        </div>
+                        <button
+                            onClick={() => reset()}
+                            className="text-gray-950 text-sm bg-gray-300 rounded-full px-2"
+                        >
+                            clear                            
+                        </button>
                     </div>
-                    {users?.map(user => (
+                    <SearchUser searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                    {filteredUsers?.map(user => (
                         <div
                             key={user._id}
                             className="flex items-center justify-between mb-6"
