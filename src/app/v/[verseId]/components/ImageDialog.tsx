b0769479ebc38ff0362f341ebff575b5c8709ed9
Blair Fleming <blair3003.bf@gmail.com>
2023-09-13 17:15:54 +0100

    Update ImageDialog.tsx

--- src/app/v/[verseId]/components/ImageDialog.tsx
+++ src/app/v/[verseId]/components/ImageDialog.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { useEffect, useRef, Dispatch, SetStateAction } from 'react'
+import { useEffect, useRef, Dispatch, SetStateAction, useCallback } from 'react'
 import { HiXMark } from 'react-icons/hi2'
 
 interface Props {
@@ -22,20 +22,20 @@ const ImageDialog = ({ image, setImage }: Props) => {
     }, [image])
 
     return (
-        <dialog ref={dialogRef} className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10 rounded-lg backdrop:bg-gray-950/50">
+        <dialog ref={dialogRef} className="fixed top-50 left-50 -translate-x-50 -translate-y-50 z-10 rounded-lg backdrop:bg-gray-950/50 shadow-xl">
             {image && (
                 <Image
                     alt='Image'
                     src={image}
                     height={500}
                     width={500}
-                    className="rounded-lg cursor-pointer"
+                    className="cursor-pointer"
                 />
             )}
             <button
                 type="button"
                 onClick={() => setImage('')}
-                className="absolute rounded-full text-gray-950 bg-white/50 top-4 right-4"
+                className="absolute rounded-full text-gray-950 bg-white/50 top-4 right-4 shadow"
             >
                 <HiXMark size={24} />                                
             </button>
